4eee8933ee49ce28b1389508570eb81cc8c5d3ec
sahilister <sahil.dhiman27091999@gmail.com>
2019-11-29 06:49:07 +0530

    Refactored the code from html/css to react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return(
+        <div>
+            <h2 id="quote">Random Quote</h2>
+            <h4 id="author">- Author</h4>
+        </div>
+    )
+}
+
+export default App
